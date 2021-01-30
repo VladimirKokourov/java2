32a4b20297d2b7b91856f7272cc8dff5b4b14f76
VladimirKokourov <vkokourov@gmail.com>
2021-01-30 19:52:53 +0700

    Homework #5

---
+++ MainCompare.java
@@ -0,0 +1,62 @@
+package ru.geekbrains.homework.lesson5;
+
+import java.util.Arrays;
+
+public class MainCompare {
+
+    static final int SIZE = 10000000;
+    static final int HALF = SIZE / 2;
+
+    public static void main(String[] args) {
+        float[] arr = new float[SIZE];
+        method1(arr);
+        method2(arr);
+    }
+
+    public static void method1(float[] arr) {
+        Arrays.fill(arr, 1);
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.out.println(System.currentTimeMillis() - a);
+    }
+
+    public static void method2(float[] arr) {
+        Arrays.fill(arr, 1);
+        long a = System.currentTimeMillis();
+        float[] a1 = new float[HALF];
+        float[] a2 = new float[HALF];
+
+        System.arraycopy(arr, 0, a1, 0, HALF);
+        System.arraycopy(arr, HALF, a2, 0, HALF);
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < a1.length; i++) {
+                a1[i] = (float) (a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < a2.length; i++) {
+                a2[i] = (float) (a2[i] * Math.sin(0.2f + i+HALF / 5) * Math.cos(0.2f + i+HALF / 5) * Math.cos(0.4f + i+HALF / 2));
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        try{
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        System.arraycopy(a1, 0, arr, 0, HALF);
+        System.arraycopy(a2, 0, arr, HALF, HALF);
+
+        System.out.println(System.currentTimeMillis() - a);
+    }
+
+}
